3b210b4e26afa8ed016044bac41642b78c02b414
Lucas Pessoa <lcppcoding@gmail.com>
2021-06-07 16:53:27 -0300

    removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/create'
-  get 'reviews/update'
-  get 'reviews/destroy'
   devise_for :users
   root to: 'cities#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
