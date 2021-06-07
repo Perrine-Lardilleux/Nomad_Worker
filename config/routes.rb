d55a0bb15007d8e3e53a920e0965034c0c3121e8
Lucas Pessoa <lcppcoding@gmail.com>
2021-06-07 16:45:29 -0300

    controllers and policy updated, review#update still to do

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
+  get 'reviews/update'
+  get 'reviews/destroy'
   devise_for :users
   root to: 'cities#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
