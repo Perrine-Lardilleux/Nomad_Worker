c131a4ea8df82d56dd955794b746e20c0cd73618
Lucas Pessoa <lcppcoding@gmail.com>
2021-06-07 19:39:50 -0300

    reviews working, only missing update feature

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module NomadWorker
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    # Remote / AJAX / Forms
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
