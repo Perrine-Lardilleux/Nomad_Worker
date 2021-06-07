90f97c342055d79c063bfa9a4b3f16709c26cc37
Lucas Pessoa <lcppcoding@gmail.com>
2021-06-07 15:00:27 -0300

    added initial validations to models

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,5 @@
 class City < ApplicationRecord
   belongs_to :country
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,5 @@
 class Country < ApplicationRecord
+  has_many :cities
+  has_many :reviews, through: :cities
+  validates :name, :language, :currency, presence: true
 end
