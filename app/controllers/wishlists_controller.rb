e7fdf910c7f88113369dcad553e7d1947d937422
Thiago Youssef <thiagodyoussef@gmail.com>
2021-06-07 17:11:02 -0300

    Wishlist index

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,2 +1,6 @@
 class WishlistsController < ApplicationController
+   def index
+    skip_policy_scope
+    @wishlists = Wishlist.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     resources :reviews, only: [:create, :update, :destroy]
     resources :wishlists, only: [:create, :destroy]
   end
+  resources :wishlists, only: [:index]
 end
