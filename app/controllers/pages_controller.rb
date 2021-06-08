1101838c6978ff9d1825516140154c302ac76e5b
Thiago Youssef <thiagodyoussef@gmail.com>
2021-06-08 16:36:31 -0300

    Pages pundit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    skip_authorization
   end
 
   def economical
-    skip_authorization
     @city = City.find(params[:city_id])
     @data = @city.data
   end
 
   def expensive
-    skip_authorization
     @city = City.find(params[:city_id])
     @data = @city.data
   end
