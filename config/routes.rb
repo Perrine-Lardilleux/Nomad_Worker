e7175f48e38d28811aebb3a7cd09a0c017618f06
Lucas Pessoa <lcppcoding@gmail.com>
2021-06-10 01:27:24 -0300

    fixed wishlist index and refactored shared stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :cities, only: [:show, :index] do
     resources :reviews, only: [:create, :update, :destroy]
     resources :wishlists, only: [:create, :destroy]
     get '/economical', to: 'pages#economical', as: 'economical'
     get '/expensive', to: 'pages#expensive', as: 'expensive'
   end
+
   resources :wishlists, only: [:index]
+
   get 'my_profile', to: 'pages#my_profile', as: 'my_profile'
+
   resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
-
-  delete 'delete_wishlist', to: 'wishlists#destroy_wishlist', as: :destroy_wishlist
   
   require 'sidekiq/web'
   authenticate :user, ->(user) { user.admin? } do
