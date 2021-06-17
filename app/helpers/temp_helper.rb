d495f69c4cdba41aa576cc9b06edd1d414e80df2
Lucas Pessoa <lcppcoding@gmail.com>
2021-06-17 02:02:59 -0300

    temp helper refactored

--- app/helpers/temp_helper.rb
+++ app/helpers/temp_helper.rb
@@ -1,17 +1,14 @@
 module TempHelper
-  def summer_average(city)
-    arr = get_temps(city)
-    city.latitude >= 0 ? arr[5, 3].sum / 3 : (arr[0, 2].sum + arr[-2]) / 3
-  end
-
-  def winter_average(city)
-    arr = get_temps(city)
-    city.latitude >= 0 ? (arr[0, 2].sum + arr[-2]) / 3 : arr[5, 3].sum / 3
-  end
-
-  private
-
-  def get_temps(city)
-    city.weather['temp']
+  def temp_average(city, season)
+    arr = city.weather['temp']
+    mid_year = (arr[5, 3].sum / 3).round(1)
+    end_year = ((arr[0, 2].sum + arr[-2]) / 3).round(1)
+    if city.latitude >= 0 && season == 'summer'
+      mid_year
+    elsif city.latitude >= 0 || season == 'summer'
+      end_year
+    else
+      mid_year
+    end
   end
 end
